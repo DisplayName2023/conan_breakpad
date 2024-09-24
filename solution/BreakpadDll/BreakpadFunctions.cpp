83becba350ffdf78e734c6fe06bc7cc80768beb1
Slash 2WD <wanwei@sinolink-technologies.com>
2024-09-24 23:56:16 +0800

    rename

--- solution/BreakpadDll/BreakpadFunctions.cpp
+++ solution/BreakpadDll/BreakpadFunctions.cpp
@@ -1,24 +1,41 @@
 #include "BreakpadFunctions.h"
 
+#include <iostream>
 #include <string>
 #include <locale>
+#include <cwchar>
 #include <codecvt>
+#include <stdexcept>
+
+std::wstring utf8ToWString(const std::string& utf8_str) {
+    std::wstring wstr;
+    std::mbstate_t state = std::mbstate_t();  // Initialize the conversion state
+
+    const char* str = utf8_str.data();
+    std::size_t len = utf8_str.size();
+    wchar_t wc;
+    while (len > 0) {
+        std::size_t converted = std::mbrtowc(&wc, str, len, &state);
+        if (converted == static_cast<std::size_t>(-1) || converted == static_cast<std::size_t>(-2)) {
+            throw std::runtime_error("UTF-8 to wide conversion error");
+        }
+        wstr += wc;
+        str += converted;
+        len -= converted;
+    }
+    return wstr;
+}
 
 #if defined _WIN32
 #include <Windows.h>
 
-std::wstring stringToWstring(const std::string& str) {
+std::wstring utf8ToWStringWin64(const std::string& str) {
 	int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
 	std::wstring wstr(size_needed, 0);
 	MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstr[0], size_needed);
 	return wstr;
 }
 
-#else
-std::wstring stringToWstring(const std::string& str) {
-	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-	return converter.from_bytes(str);
-}
 #endif
 
 namespace BreakpadLib
@@ -32,7 +49,7 @@ extern "C"
 
 	void initBreakpad(const char* dumpDirName)
 	{
-		std::wstring ws = stringToWstring(dumpDirName);
+		std::wstring ws = utf8ToWString(dumpDirName);
 		BreakpadLib::InitializeBreakpad(ws.c_str());
 	}
 
